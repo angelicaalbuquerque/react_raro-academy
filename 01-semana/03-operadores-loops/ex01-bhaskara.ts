b7eca2f510f93771585648749c4c5d45a94e9785
angelicaalbuquerque <angelica.o.albuquerque@gmail.com>
2022-03-02 16:52:08 -0300

    Correração: alteração de variáveis de let para const

--- 01-semana/03-operadores-loops/ex01-bhaskara.ts
+++ 01-semana/03-operadores-loops/ex01-bhaskara.ts
@@ -1,14 +1,14 @@
 // 1. Faça um algoritmo que calcule a fórmula de equação quadrática ("fórmula de bháskara"). Imagino que você fará bom uso da https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math
 
 alert("Vamos calcular a fórmula de Bháskara?");
-let valorA: number = Number(prompt("Informe o valor de A: "));
-let valorB: number = Number(prompt("Agora, informe o valor de B: "));
-let valorC: number = Number(prompt("Por último, informe o valor de C: "));
+const valorA: number = Number(prompt("Informe o valor de A: "));
+const valorB: number = Number(prompt("Agora, informe o valor de B: "));
+const valorC: number = Number(prompt("Por último, informe o valor de C: "));
 
-let delta = valorB * valorB - 4 * valorA * valorC;
+const delta = valorB * valorB - 4 * valorA * valorC;
 
-let resultadoPositivo = (-valorB + Math.sqrt(delta)) / (2 * valorA);
-let resultadoNegativo = (-valorB - Math.sqrt(delta)) / (2 * valorA);
+const resultadoPositivo = (-valorB + Math.sqrt(delta)) / (2 * valorA);
+const resultadoNegativo = (-valorB - Math.sqrt(delta)) / (2 * valorA);
 
 if (delta < 0) {
   alert("Não pode ser aplicada a fórmula. Delta não pode ser igual a 0");
