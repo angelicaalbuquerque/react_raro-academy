b983d7f25edc3b0af1a22fe3d8c8b79dfff79144
angelicaalbuquerque <angelica.o.albuquerque@gmail.com>
2022-03-10 23:23:20 -0300

    Prática - Promises

---
+++ 02-semana/Exercícios/ex04-revisao/respostas/fromPairs.ts
@@ -0,0 +1,18 @@
+import { KeyAndValue } from "../../types/keyAndValue";
+
+/**
+ * Deve criar um objeto a partir da lista de arrays chave/valor
+ * 
+ * Ex.: fromPairs(['a', 'a'], ['b': 1], ['c': false]) === { a: 'a', b: 1, c: false }
+ * 
+ * @param args 
+ * @returns 
+ */
+export const fromPairs = (args: KeyAndValue[]): Record<string, unknown> => {
+  const objFromPairs: Record<string, unknown> = {};
+  args.forEach((keyValue) => {
+    const [key, value] = keyValue;
+    objFromPairs[key] = value;
+  })
+  return objFromPairs;
+};
