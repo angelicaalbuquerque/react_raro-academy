63f0bbde1c72da2c2e0976a8f23cbde902c484fe
angelicaalbuquerque <angelica.o.albuquerque@gmail.com>
2022-03-11 23:56:14 -0300

    Prática - revisão da semana

---
+++ 02-semana/Exercícios/ex04-revisao/respostas/fila.ts
@@ -0,0 +1,65 @@
+import { writeFile, readFile } from "fs";
+import { resolve } from "path";
+
+const ARQUIVO_DE_FILA = `${resolve(".")}/files/fila.txt`;
+
+/**
+ * Os métodos escritos abaixo implementam uma fila de mensagens escritas em
+ * arquivo de texto, presente na pasta "files". A cada mensagem escrita nesta fila,
+ * (através do método `escreveNaFila`) o código escreve a frase ao final do arquivo.
+ * O método `consumirDafila` retira a primeira mensagem escrita no arquivo e a retorna.
+ *
+ * As funções abaixo estão todas escritas utilizando callbacks como soluções
+ * para a manipulação dos arquvos.
+ *
+ * Tranforme a solução escrita abaixo em um código válido utilizando promises ou
+ * async/await.
+ */
+
+export async function zerarAquivo(): Promise<void> {
+  return escreveArquivo("");
+}
+
+export async function leArquivo(): Promise<string> {
+  return new Promise((resolve, reject) => {
+    readFile(ARQUIVO_DE_FILA, "utf8", (err, resultado) => {
+      if (err) {
+        return reject(err);
+      }
+      resolve(resultado);
+    });
+  });
+}
+
+export async function escreveArquivo(texto: string): Promise<void> {
+  return new Promise((resolve, reject) => {
+    writeFile(ARQUIVO_DE_FILA, texto, "utf8", function (err) {
+      if (err) {
+        return reject(err);
+      }
+      resolve();
+    });
+  });
+}
+
+export async function escreveNaFila(texto: string): Promise<void> {
+  const textoAtual = await leArquivo();
+
+  console.log("texto encontrado anteriormente no arquivo", textoAtual);
+  const novoTexto = textoAtual ? `${textoAtual}\n${texto}` : texto;
+
+  await escreveArquivo(novoTexto);
+  console.log("texto escrito no arquivo");
+}
+
+export async function consumirDaFila(): Promise<string> {
+  const textoAtual = await leArquivo();
+
+  console.log("texto encontrado anteriormente no arquivo", textoAtual);
+  const [linhaConsumida, ...linhas] = textoAtual.split("\n");
+  console.log("======== linha consumida", linhaConsumida);
+
+  await escreveArquivo(linhas.join("\n"));
+  console.log("texto escrito no arquivo");
+  return linhaConsumida;
+}
--- 02-semana/Exercícios/ex04-revisao/respostas/uniq.ts
+++ 02-semana/Exercícios/ex04-revisao/respostas/uniq.ts
@@ -1,15 +1,15 @@
 /**
  * Retorna um array com todos os elementos únicos.
- * 
+ *
  * > Importante: Os parâmetros não devem ter sua estrutura alterada.
- * 
+ *
  * @param items array com itens de qualquer tipo.
- * 
+ *
  * @returns somente os itens definidos.
  */
 export const uniq = <T>(args: T[]): T[] => {
   const arrayUniq = [];
-  args.forEach(x => {
+  args.forEach((x) => {
     if (!arrayUniq.includes(x)) {
       arrayUniq.push(x);
     }
